5e53ace3a1a887232de2dfc19d8de21b184e7874
Samuel Lee <samuelxlee@gmail.com>
2021-01-13 21:13:00 -0800

    wrote all the endpoints for the users-router

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,40 +2,71 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
-  // this needs a middleware to check that the request body is valid
+const Users = require("./users-model");
+const Posts = require("../posts/posts-model");
+
+const { validateUserId, validateUser, validatePost } = require("../middleware/middleware");
+
+router.post('/', validateUser, (req, res, next) => {
+  Users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(next);
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get('/', (req, res, next) => {
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(next);
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify user id
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify user id
+router.delete('/:id', validateUserId, (req, res, next) => {
+  Users.remove(req.params.id)
+    .then(count => {
+      res.status(200).json({ message: "the user has been nuked" });
+    })
+    .catch(next);
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  Users.update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(next);
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.post('/:id/posts', validateUserId, validatePost, (req, res, next) => {
+  const postInfo = { ...req.body, user_id: req.params.id }
+
+  Posts.insert(postInfo)
+    .then(post => {
+      res.status(210).json(post);
+    })
+    .catch(next);
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-  // this needs a middleware to verify user id
+router.get('/:id/posts', validateUserId, (req, res, next) => {
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(next);
 });
 
-// do not forget to export the router
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    info: "something went wrong inside the users router",
+    message: err.message,
+    stack: err.stack
+  });
+});
+
+module.exports = router;
